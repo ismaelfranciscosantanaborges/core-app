51a510f0f6af7d8ef23e66e292753e37fd462db2
Ismael Santana <ismaelfrancisco2000@gmail.com>
2024-11-11 09:18:00 -0400

    feat: :sparkles: local_db now is visible

--- lib/core/abstractions/index.dart
+++ lib/core/abstractions/index.dart
@@ -1,4 +1,5 @@
 export './http/http.dart';
-export './http/kresponse_type.dart';
 export './http/http_dio.dart';
+export './http/kresponse_type.dart';
 export './http/models/k_http_response.dart';
+export './local_db/local_db.dart';
